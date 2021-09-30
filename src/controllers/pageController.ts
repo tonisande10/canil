4edf2bb9a8d8e2ea5308a071060d5a07ffed3b66
Antonio Sande <toni_sande@hotmail.com>
2021-09-30 08:38:07 -0300

    sagundo commit

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,23 +1,57 @@
 import { Request, Response } from 'express';
-
+import {creatMenuObject} from '../helpers/CreateMenuObjects';
+import { Pet } from '../models/pet';
 
 export const home = (req: Request, res: Response)=>{
+
+let list = Pet.getAll();
+
 res.render('pages/page',{
+    menu: creatMenuObject('all'),
+  
     banner:{
         title: 'Todos os animais',
-        background: 'allanimals.jpg'}
+        background: 'allanimals.jpg'
+    },
+    list
 
     });
+
 }
 export const dogs = (req: Request, res: Response)=>{
-    res.send('antonio');
-    //res.render('pages/page');
+    let list = Pet.getFromType('dog');
+  
+    res.render('pages/page',{
+
+        menu: creatMenuObject('dog'),
+        banner:{
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'},
+
+    list
+        });
 }
 export const cats = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('cat');
+    res.render('pages/page',{
+        menu:creatMenuObject('cat'),
+        banner:{
+            title: 'Gatos',
+            background: 'banner_cat.jpg'},
 
-    //res.render('pages/page');
+            list
+    
+        });
 }
 export const fishes = (req: Request, res: Response)=>{
+    let list = Pet.getFromType('fish');
+    res.render('pages/page',{
 
-    //res.render('pages/page');
+        menu:creatMenuObject('fish'),        
+        banner:{
+            title: 'Peixes',
+            background: 'banner_fish.jpg'},
+            list
+    
+        });
 }
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
-import { send } from 'process';
+import { Pet } from '../models/pet';
+import {creatMenuObject} from '../helpers/CreateMenuObjects';
 
 
 export const search = (req: Request, res: Response)=>{
-    res.send('antonio');
+    let query: string = req.query.q as string;
+    let list = Pet.getFromName(query);
+    res.render('pages/page', {
+        
+menu: creatMenuObject(''),
+list
+
+
+    });
 
     //res.render('pages/page');
 }
